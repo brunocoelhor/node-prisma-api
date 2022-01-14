bea05d26a38ef9b9714bf76b3d734649b1c59888
Bruno Coelho Rodrigues <brunocoelho.r@gmail.com>
2022-01-14 10:54:23 -0300

    Add create delivery

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/CreateClientController';
+import { CreateDeliveryController } from './modules/deliveries/useCases/CreateDeliveryController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/CreateDeliverymanController';
 
 const routes = Router();
@@ -11,11 +12,13 @@ const authenticateDeliverymanController = new AuthenticateDeliverymanController(
 
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
+const createDeliveryController = new CreateDeliveryController();
 
 routes.post('/client/authenticate', authenticateClienteController.handle);
 routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle);
 
 routes.post('/client', createClientController.handle);
 routes.post('/deliveryman', createDeliverymanController.handle);
+routes.post('/delivery', createDeliveryController.handle);
 
 export { routes };
