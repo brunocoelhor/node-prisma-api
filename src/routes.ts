76039ef950e6b0bfb02657c909a18f074ee23c0f
Bruno Coelho Rodrigues <brunocoelho.r@gmail.com>
2022-01-17 17:14:18 -0300

    List deliveries available and deliveryman auth

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { ensureAuthenticateClient } from './middlewares/ensureAuthenticateClient';
+import { ensureAuthenticateDeliveryman } from './middlewares/ensureAuthenticateDeliveryman';
 import { AuthenticateClientController } from './modules/account/authenticateClient/AuthenticateClientController';
 import { AuthenticateDeliverymanController } from './modules/account/authenticateDeliveryman/AuthenticateDeliverymanController';
 import { CreateClientController } from './modules/clients/useCases/CreateClientController';
-import { CreateDeliveryController } from './modules/deliveries/useCases/CreateDeliveryController';
+import { CreateDeliveryController } from './modules/deliveries/useCases/createDelivery/CreateDeliveryController';
+import { FindAllAvailableController } from './modules/deliveries/useCases/findAllAvailable/findAllAvailableController';
 import { CreateDeliverymanController } from './modules/deliveryman/useCases/CreateDeliverymanController';
 
 const routes = Router();
@@ -13,13 +15,17 @@ const authenticateDeliverymanController = new AuthenticateDeliverymanController(
 
 const createClientController = new CreateClientController();
 const createDeliverymanController = new CreateDeliverymanController();
+
 const createDeliveryController = new CreateDeliveryController();
+const findAllAvailable = new FindAllAvailableController();
 
 routes.post('/client/authenticate', authenticateClienteController.handle);
 routes.post('/deliveryman/authenticate', authenticateDeliverymanController.handle);
 
 routes.post('/client', createClientController.handle);
 routes.post('/deliveryman', createDeliverymanController.handle);
+
 routes.post('/delivery', ensureAuthenticateClient, createDeliveryController.handle);
+routes.get('/delivery/available', ensureAuthenticateDeliveryman, findAllAvailable.handle);
 
 export { routes };
